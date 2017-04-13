65464d120987ce207ed3c4535fc7e41a786acf78
Xin Meng <mengxin.city@gmail.com>
2017-04-13 17:13:07 +0100

    add component and use multiple theme for different md in same app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdAutocompleteModule, MdButtonModule, MdButtonToggleModule, MdCardModule, MdCheckboxModule, MdChipsModule,
+  MdCoreModule,
+  MdDialogModule, MdGridListModule, MdIconModule,
+  MdInputModule,
+  MdLineModule, MdListModule, MdMenuModule, MdOptionModule, MdProgressBarModule, MdProgressSpinnerModule, MdRadioModule,
+  MdRippleModule,
+  MdSelectionModule,
+  MdSelectModule, MdSidenavModule, MdSliderModule, MdSlideToggleModule, MdSnackBarModule, MdTabsModule, MdToolbarModule,
+  MdTooltipModule,
+  OverlayContainer
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    MdSelectModule,
+    MdDialogModule,
+    MdAutocompleteModule,
+    MdCardModule,
+    MdChipsModule,
+    MdLineModule,
+    MdMenuModule,
+    MdIconModule,
+    MdListModule,
+    MdOptionModule,
+    ReactiveFormsModule,
+    MdButtonToggleModule,
+    MdGridListModule,
+    MdProgressSpinnerModule,
+    MdSlideToggleModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdSnackBarModule,
+    MdProgressBarModule,
+    MdTooltipModule,
+    MdTabsModule,
+    MdSliderModule,
+    MdSelectionModule,
+    MdRippleModule,
+    MdRadioModule,
+    MdCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public overlayContainer: OverlayContainer) {
+    overlayContainer.themeClass = 'red-indigo';
+  }
+}
